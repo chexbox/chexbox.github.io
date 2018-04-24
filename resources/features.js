b6df7141002a0226b1d158dc6f23a1e478dcb497
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-24 16:10:54 -0500

    Create features.js

---
+++ resources/features.js
@@ -0,0 +1,24 @@
+
+function menutoggle() {
+    'use strict';
+    document.getElementById("settings").classList.remove("show");
+    document.getElementById("drop").classList.toggle("show");
+    document.getElementById("mainbar").classList.toggle("fixed");
+};
+function settingstoggle() {
+    'use strict';
+    document.getElementById("drop").classList.remove("show");
+    document.getElementById("settings").classList.toggle("show");
+    document.getElementById("mainbar").classList.toggle("fixed");
+};
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    'use strict';
+    if (!(event.target == document.getElementById("drop")) && !(event.target == document.getElementById("dropbtn")) && !(event.target == document.getElementById("settings")) && !(event.target == document.getElementById("settingsbtn"))) {
+
+        document.getElementById("drop").classList.remove("show");
+        document.getElementById("settings").classList.remove("show");
+        document.getElementById("mainbar").classList.remove("fixed");
+    }
+}
+/*Inspired and based on a W3Schools tutorial.*/
